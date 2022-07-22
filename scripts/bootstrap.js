0164a07ce3cf6a47e08ff6e596304d4e82d67394
jordendickerson <jordendickerson24@gmail.com>
2022-07-21 20:00:22 -0600

    Created game functionality

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,17 +1,70 @@
+let gameOver = false;
+let winner = '';
+
+const gameStatus = document.querySelector('.game-status');
+const resetButton = document.querySelector('.reset');
 
 const gameBoard = (() => {
     const gameTiles = document.querySelectorAll('.game-tile');
-    let gameTilesArray = ['X','O','X','O','X','O','O','X','X']
+    let gameTilesArray = ['','','','','','','','','']
     
 
-    let index = 0
-    const changeTiles = function() {
+    let turn = 'X';
+    //Update Tiles
+    const updateTiles = function() {
         gameTiles.forEach(tile => {
             tile.innerHTML = gameTilesArray[tile.dataset.tile];
         })
     }
 
-    return {changeTiles};
+    //On Tile Click
+    const onTileClick = function(tile) {
+        if (!tile.innerHTML && !gameOver){
+            gameTilesArray[tile.dataset.tile] = turn;
+            if (turn === 'X')
+                turn = 'O';
+            else
+                turn = 'X';
+            update();
+        }
+    }
+
+    gameTiles.forEach(tile => {
+        tile.addEventListener('click', () => onTileClick(tile));
+    })
+
+    //Check for win
+    const checkWin = function(combinationArray){
+        if (gameTilesArray[combinationArray[0]] === gameTilesArray[combinationArray[1]] && gameTilesArray[combinationArray[1]] === gameTilesArray[combinationArray[2]] && gameTilesArray[combinationArray[0]]){
+            gameOver = true;
+            winner = gameTilesArray[combinationArray[0]];
+        }
+    }
+
+    //Reset
+    const reset = function(){
+        gameTilesArray = ['','','','','','','','',''];
+        turn = 'X';
+        gameOver = false;
+        winner = '';
+        gameStatus.textContent = '';
+        update();
+    }
+
+    return {updateTiles, checkWin, reset};
 })();
 
-gameBoard.changeTiles();
+function update() {
+    gameBoard.updateTiles();
+    
+    const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    winningCombos.forEach(combo => {
+        gameBoard.checkWin(combo);
+    })
+
+    if (gameOver){
+        gameStatus.textContent = winner + ' is the winner!';
+    }
+}
+
+resetButton.addEventListener('click', () => gameBoard.reset())
